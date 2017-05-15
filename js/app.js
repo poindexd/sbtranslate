a780779e8e2d81ca8b05ba1044961f88e5ec4432
Haemin Park <haeminp@umich.edu>
2017-05-15 12:44:40 -0400

    bad json data

--- js/app.js
+++ js/app.js
@@ -3,19 +3,40 @@ angular.module('app', ['jsonFormatter'])
 	
 
 	$scope.slideMap = {
-		2: 'image_likert'
+		2: 'multiple_choice',
+		4: 'multiple_choice',
+		5: 'text',
+		25: 'multiple_choice',
+		26: 'multiple_choice',
+		1: 'multiple_choice',
+		31: 'likert',
+		34: 'likert',
+		37: 'multiple_choice',
+		12: 'likert',
+		30: 'multiple_choice',
+		27: 'likert',
+		35: 'text',
+		11: 'likert',
+		23: 'likert',
+		28: 'likert',
+		33: 'likert',
+		36: 'multiple_choice',
+		38: 'text',
+		10: 'likert'
 	}
 
 	$scope.translate = function(data){
 		return data.map(function(o){
 			var n = {};
+				if ($scope.slideMap[o.slide_type_id] == null) {
+					return null;
+				}
+
 				if (o.buttons && o.buttons.length){
 					n.options = o.buttons.map(function(b){
-						return {weight: 0, text: b.text, correct: o.slide_type_id==1 || o.slide_type_id==4 || o.slide_type_id==2}
+					 	return {weight: 0, text: b.text, correct: o.slide_type_id==1 || o.slide_type_id==4 || o.slide_type_id==2}
 					});
 				}
-				
-				n.template = $scope.slideMap[o.slide_type_id]
 
 				n.content = o.contents.description ? $(o.contents.description).text() :
 										o.contents.question ? $(o.contents.question).text() :
@@ -25,7 +46,13 @@ angular.module('app', ['jsonFormatter'])
 				if (o.image && !o.image.includes('missing')){
 					n.image_url = 'https://qa.wellopp.com' + o.image;
 					n.image_align = Math.random() > 0.5 ? 'Left' : 'Right';
+					n.template = $scope.slideMap[o.slide_type_id] + (Math.random() > 0.5 ? "_image_right" : "_image_left");
+					
+				} else { //image doesn't exist
+
+					n.template = $scope.slideMap[o.slide_type_id] + "_image_none"
 				}
+
 				if (o.audio && !o.audio.includes('missing')){
 					n.audio = o.audio;
 				}
@@ -43,7 +70,8 @@ angular.module('app', ['jsonFormatter'])
 	$scope.input = {};
 	$scope.output = {};
 
-	var URL = "https://sb.wellopp.com/api/v1/surveys/264/slides.json";
+	var URL = "https://sb.wellopp.com/api/v1/surveys/242/slides.json";
+
 	$scope.init = function(){
 		$.getJSON(URL, function(data){
 			console.log(data)
